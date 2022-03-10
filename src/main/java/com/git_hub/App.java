c3f0e6031baac8c1fa31bf66c85621739060390e
rohit <kamrarohit97@gmail.com>
2022-03-10 14:59:01 +0530

    added git-demo to local repo

---
+++ src/main/java/com/git_hub/App.java
@@ -0,0 +1,13 @@
+package com.git_hub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
