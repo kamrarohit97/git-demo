fbaaab0b6a4255f8750c7dfbf356557c3eb27523
kamrarohit97 <101322723+kamrarohit97@users.noreply.github.com>
2022-03-10 16:15:03 +0530

    method added by developer 2

--- src/main/java/com/git_hub/App.java
+++ src/main/java/com/git_hub/App.java
@@ -9,6 +9,9 @@ public class App
 		if(data.equals(Constant.DATA_TYPE));
 		System.out.println("welcome");
 	}
+	public void method2(){
+	System.out.println("method2");
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
